322f835c882220e148dfab983d58b56fa26387d9
yhshin0 <asyh1047@naver.com>
2021-12-14 18:41:19 +0900

    feat: getOembedSpec() 에러 처리
    - oEmbed spec 데이터 가져올 때 에러 발생 시 에러 처리
    - getOembedSpec() 생성자에서 제거
    resolves: #1

---
+++ src/oembed/constants/oembed-error-message.ts
@@ -0,0 +1,3 @@
+export const OEMBED_ERROR_MSG = {
+  FAIL_TO_FETCH_OEMBED_SPEC: 'spec 데이터를 가져오는 데 실패하였습니다',
+};
--- src/oembed/oembed.service.ts
+++ src/oembed/oembed.service.ts
@@ -1,21 +1,24 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { lastValueFrom, map } from 'rxjs';
 
+import { OEMBED_ERROR_MSG } from './constants';
 import { OembedSpecList } from './interfaces';
 
 @Injectable()
 export class OembedService {
   oembedSpecList: Promise<OembedSpecList[]>;
-  constructor(private httpService: HttpService) {
-    this.oembedSpecList = this.getOembedSpec();
-  }
+  constructor(private httpService: HttpService) {}
 
   async getOembedSpec(): Promise<OembedSpecList[]> {
     const oembedSpecUrl = 'https://oembed.com/providers.json';
     const observer = this.httpService
       .get(oembedSpecUrl)
       .pipe(map((axiosResponse) => axiosResponse.data));
-    return await lastValueFrom(observer);
+    return await lastValueFrom(observer).catch(() => {
+      throw new InternalServerErrorException(
+        OEMBED_ERROR_MSG.FAIL_TO_FETCH_OEMBED_SPEC,
+      );
+    });
   }
 }
