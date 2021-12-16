6ad8e707f71b883639576e0d183d39c66a71a3cf
yhshin0 <asyh1047@naver.com>
2021-12-17 00:00:51 +0900

    feat: facebook oembed
    - facebook oembed data를 가져올 때 app_id와 client_token이 필요하기 때문에 이를 url에 덧붙여서 가져올 수 있도록 구현
    resolves: #10

--- src/oembed/oembed.controller.ts
+++ src/oembed/oembed.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+
 import { UrlDto } from './dto/url.dto';
 import { OembedService } from './oembed.service';
 
@@ -7,7 +8,7 @@ export class OembedController {
   constructor(private oembedService: OembedService) {}
 
   @Post()
-  async getOembedData(@Body() urlDto: UrlDto): Promise<string> {
+  async getOembedData(@Body() urlDto: UrlDto): Promise<any> {
     return await this.oembedService.getOembedData(urlDto);
   }
 }
--- src/oembed/oembed.module.ts
+++ src/oembed/oembed.module.ts
@@ -1,5 +1,7 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { OembedController } from './oembed.controller';
 import { OembedService } from './oembed.service';
 
@@ -9,6 +11,7 @@ import { OembedService } from './oembed.service';
       timeout: 5000,
       maxRedirects: 5,
     }),
+    ConfigModule,
   ],
   controllers: [OembedController],
   providers: [OembedService],
