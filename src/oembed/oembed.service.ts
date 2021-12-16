d53e2344126bc3391302c790fab9b3ba1f9a422d
yhshin0 <asyh1047@naver.com>
2021-12-16 20:34:08 +0900

    feat: oembed spec setting
    - oembed spec endpoint url과 schemes를 배열로 분리
    - schemes의 문자열 중 .과 *를 각각 \., \w 로 치환하여 정규표현식으로 저장
    - endpoint url의 {format} 부분을 json으로 치환하여 저장
    - 속성명 변경(oembedSpecList -> oembedSpeccEndpointUrlAndSchemesList)
    - 함수명 변경(setOembedSpecList -> setOembedSpecEndpointUrlAndSchemesList)
    resolves: #8

--- src/oembed/oembed.service.ts
+++ src/oembed/oembed.service.ts
@@ -4,11 +4,11 @@ import { lastValueFrom, map } from 'rxjs';
 
 import { OEMBED_ERROR_MSG } from './constants';
 import { UrlDto } from './dto/url.dto';
-import { OembedSpecList } from './interfaces';
+import { OembedSpecEndpointUrlAndSchemes, OembedSpecList } from './interfaces';
 
 @Injectable()
 export class OembedService {
-  oembedSpecList: OembedSpecList[];
+  oembedSpecEndpointUrlAndSchemesList: OembedSpecEndpointUrlAndSchemes[];
   constructor(private httpService: HttpService) {}
 
   async getOembedSpecList(): Promise<OembedSpecList[]> {
@@ -23,15 +23,33 @@ export class OembedService {
     });
   }
 
-  async setOembedSpecList(): Promise<void> {
-    if (this.oembedSpecList) {
+  async setOembedSpecEndpointUrlAndSchemesList(): Promise<void> {
+    if (this.oembedSpecEndpointUrlAndSchemesList) {
       return;
     }
-    this.oembedSpecList = await this.getOembedSpecList();
+
+    const specList = await this.getOembedSpecList();
+    this.oembedSpecEndpointUrlAndSchemesList = [];
+
+    for (const spec of specList) {
+      for (const endpoint of spec.endpoints) {
+        const schemesList = [];
+
+        endpoint.schemes?.map((scheme) => {
+          const newScheme = scheme.replace(/\./g, '\\.').replace(/\*/g, '\\w');
+          schemesList.push(new RegExp(newScheme));
+        });
+
+        this.oembedSpecEndpointUrlAndSchemesList.push({
+          schemes: schemesList,
+          url: endpoint.url.replace('{format}', 'json'),
+        });
+      }
+    }
   }
 
   async getOembedData(urlDto: UrlDto): Promise<string> {
-    await this.setOembedSpecList();
+    await this.setOembedSpecEndpointUrlAndSchemesList();
 
     const urlStartIndex = urlDto.url.indexOf('//') + 2;
     const url = urlDto.url.substring(urlStartIndex);
