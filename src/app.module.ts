364c50925468b0059bc3b29d85e580dd6c92f45f
yhshin0 <asyh1047@naver.com>
2021-12-16 23:39:10 +0900

    feat: ConfigModule 설정
    - AppModule에 ConfigModule import
    resolves: #10

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OembedModule } from './oembed/oembed.module';
 
 @Module({
-  imports: [OembedModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    OembedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
