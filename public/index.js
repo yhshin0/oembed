ac702d839882f80df498a978b05bc607f22f3540
yhshin0 <asyh1047@naver.com>
2021-12-17 21:21:09 +0900

    feat: a, img 태그 구현
    - url이나 썸네일에 대해 a 태그, img 태그 적용
    resolves: #12

--- public/index.js
+++ public/index.js
@@ -9,11 +9,20 @@ document.getElementById('submit-btn').addEventListener('click', () => {
       document.getElementById('result').innerHTML =
         '<table><tbody></tbody></table>';
       for (const key of Object.keys(response)) {
+        let value = response[key];
+        if (
+          key === 'thumbnail_url' ||
+          key === 'thumbnail_url_with_play_button'
+        ) {
+          value = `<img src='${value}' />`;
+        } else if (key.includes('url')) {
+          value = `<a href='${value}'>${value}</a>`;
+        }
         document
           .querySelector('#result>table>tbody')
           .insertAdjacentHTML(
             'beforeend',
-            `<tr><td>${key}</td><td>${response[key]}</td></tr>`,
+            `<tr><td>${key}</td><td>${value}</td></tr>`,
           );
       }
     })
