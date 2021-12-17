f0a8cfe98c6532b2fd9986b04542ff749f14d98f
yhshin0 <asyh1047@naver.com>
2021-12-17 20:58:20 +0900

    refactor: API response code 변경
    - oEmbed API response code 200으로 반환하도록 변경

--- src/oembed/oembed.controller.ts
+++ src/oembed/oembed.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 
 import { UrlDto } from './dto/url.dto';
 import { OembedService } from './oembed.service';
@@ -8,6 +8,7 @@ export class OembedController {
   constructor(private oembedService: OembedService) {}
 
   @Post()
+  @HttpCode(200)
   async getOembedData(@Body() urlDto: UrlDto): Promise<any> {
     return await this.oembedService.getOembedData(urlDto);
   }
